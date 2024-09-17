7b8088e83272601d276248e2c5446e9574630fb5
andersbor <anbo@zealand.dk>
2024-09-17 10:50:33 +0200

    FlowRow, FlowColumn

--- app/src/main/java/com/example/screenorientationexample/MainActivity.kt
+++ app/src/main/java/com/example/screenorientationexample/MainActivity.kt
@@ -4,14 +4,15 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,28 +26,30 @@ import com.example.screenorientationexample.ui.theme.ScreenOrientationExampleThe
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ScreenOrientationExampleTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    val configuration = LocalConfiguration.current
-                    when (configuration.orientation) {
-                        Configuration.ORIENTATION_PORTRAIT -> {
-                            PortraitMessages(modifier = Modifier.padding(innerPadding))
-                        }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        val configuration = LocalConfiguration.current
+                        when (configuration.orientation) {
+                            Configuration.ORIENTATION_PORTRAIT -> {
+                                PortraitMessages()
+                            }
 
-                        Configuration.ORIENTATION_LANDSCAPE -> {
-                            LandscapeMessages(modifier = Modifier.padding(innerPadding))
-                        }
+                            Configuration.ORIENTATION_LANDSCAPE -> {
+                                LandscapeMessages()
+                            }
 
-                        else -> {
-                            Message(
-                                "Strange orientation: ${configuration.orientation}",
-                                modifier = Modifier.padding(innerPadding)
-                            )
+                            else -> {
+                                Message(
+                                    "Strange orientation: ${configuration.orientation}",
+                                )
+                            }
                         }
+                        Text(
+                            text = "Another component that is always below the messages",
+                            modifier = Modifier.padding(8.dp)
+                        )
                     }
                 }
             }
@@ -64,8 +67,10 @@ fun Message(name: String, modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class) // FlowRow is experimental
 @Composable
 fun LandscapeMessages(modifier: Modifier = Modifier) {
+    // FlowRow is a horizontal layout that wraps when there is not enough space
     Row(modifier = modifier) {
         for (i in 1..5) {
             Message(name = "Landscape $i")
@@ -73,10 +78,12 @@ fun LandscapeMessages(modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun PortraitMessages(modifier: Modifier = Modifier) {
+    // FlowColumn is a vertical layout that wraps when there is not enough space
     Column(modifier = modifier) {
-        for (i in 1..5) {
+        for (i in 1..50) {
             Message(name = "Portrait $i")
         }
     }
